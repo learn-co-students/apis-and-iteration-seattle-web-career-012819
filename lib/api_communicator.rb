e677a47a1392fda7befad371c2660c3c0df82d2c
elimerrell <elijah.merrell@gmail.com>
2019-02-11 18:33:48 -0800

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,32 +2,36 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
-  #make the web request
+def get_character_names(character_name)
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  character_films = []
+  response_hash["results"].each do |character|
+    if character_name == character['name'].downcase
+      character['films'].each do |film|
+        character_films << film
+      end
+    end
+  end
+  character_films
+end
+
+def get_character_movies_from_api(character_name)
+  character_films = get_character_names(character_name)
+  character_films.map do |film|
+    character_response_string = RestClient.get(film)
+    character_response_hash = JSON.parse(character_response_string)
+  end
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  films.each_with_index do |film, index|
+    puts "#{index + 1}. #{film['title']}"
+  end
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
